09e16c543ffb04e52297ca01e2b5ef37644631b7
Ricardo C <ricardo@suggestic.com>
2018-02-18 21:54:10 -0600

    add Genstagexample.Application to applications

--- mix.exs
+++ mix.exs
@@ -14,6 +14,7 @@ defmodule Genstagexample.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      mod: {Genstagexample.Application, []},
       extra_applications: [:logger]
     ]
   end
